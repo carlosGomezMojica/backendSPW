dd4f3122f886eec747e3d225c19e23401c2f5345
carlosGomezMojica <carlos.gomez@luxitylab.com>
2022-11-16 05:58:04 -0500

    fecha

--- modules/descuadre/model/descuadre.js
+++ modules/descuadre/model/descuadre.js
@@ -1,27 +1,26 @@
 const mongoose = require("mongoose");
-const uniqueValidator = require('mongoose-unique-validator')
+const uniqueValidator = require("mongoose-unique-validator");
 
-let Schema = mongoose.Schema
+let Schema = mongoose.Schema;
 
 let descuadreSchema = new Schema({
-    fecha:{
-        type: Date,
-        default: new Date.now(),
-        required:[true,'la fecha es requerida']
-    },
-    descripcion:{
-        type: String,
-        required: [true,'la descripcion es necesaria']
-    },
-    correccion:{
-        type: String,
-        required: [true,'la cerrocion es necesaria']
-    },
-    informe:{
-        type: String,
-        required: [true,'el informe es necesario']
-    }
-})
+  fecha: {
+    type: Date,
+    default: Date.now()
+  },
+  descripcion: {
+    type: String,
+    required: [true, "la descripcion es necesaria"],
+  },
+  correccion: {
+    type: String,
+    required: [true, "la cerrocion es necesaria"],
+  },
+  informe: {
+    type: String,
+    required: [true, "el informe es necesario"],
+  },
+});
 
-descuadreSchema.plugin(uniqueValidator,{message:'{PATH} must be unique'})
-module.exports = mongoose.model('Descuadre',descuadreSchema)
+descuadreSchema.plugin(uniqueValidator, { message: "{PATH} must be unique" });
+module.exports = mongoose.model("Descuadre", descuadreSchema);
